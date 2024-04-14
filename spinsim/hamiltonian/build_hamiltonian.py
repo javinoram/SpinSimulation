c55a679778eaa4b4ae77a833c4dbdb36e4e9e08e
jnorambu <javinorambl@gmail.com>
2024-04-14 16:32:13 -0400

    Construir matriz asociada a un operador

--- spinsim/hamiltonian/build_hamiltonian.py
+++ spinsim/hamiltonian/build_hamiltonian.py
@@ -10,6 +10,17 @@ def delta_function(a: int, b: int) -> int:
         return 1
     return 0
 
+
+""" 
+Construir las matrices de pauli para spin S
+input: 
+    - size (float): tamaño de la matriz 2*S + 1
+output:
+    - I: pauli X de spin S en formato sparse
+    - X: pauli I de spin S en formato sparse
+    - Y: pauli Y de spin S en formato sparse
+    - Z: pauli Z de spin S en formato sparse
+"""
 def X_matrix( size: int ) -> sc.sparse:
     spin = (size-1)/2.0
     base = np.zeros( (size, size) )
@@ -49,19 +60,38 @@ output:
     - Y: pauli Y de spin S en formato sparse
     - Z: pauli Z de spin S en formato sparse
 """
-def pauli_matrices(spin: float):
+def pauli_matrices(spin: float) -> dict:
     size = int( 2*spin+1 )
-    return [ I_matrix(size), X_matrix(size), Y_matrix(size), Z_matrix(size) ]
+    return { "I": I_matrix(size), "X": X_matrix(size), "Y": Y_matrix(size), "Z": Z_matrix(size) }
 
 
 
-def construct_term() -> sc.sparse:
-    return
+"""
+Construir matriz asociada al operador ingresado
+input:
+    - operadores (string): String que representa el operador, esto debe estar ordenado segun la definicion del problema
+    - spines ([float]): Lista del valor del spin en cada uno de los sitios
+output:
+    - Matriz en formato sparse del operador ingresado
+"""
+def construct_term( operator: str, spins: list) -> sc.sparse:
+    # Diccionario con las matrices de los espines no repetidos
+    spin_system = list( set(spins) )
+    matrices = { s : pauli_matrices(s) for s in spin_system }
+
+    # Construccion del operador mediante una aplicacion consecutiva del
+    # producto kronecker
+    result = matrices[ spins[0] ][operator[0]]
+    for i, op in enumerate(operator[1:]):
+        result = sc.sparse.kron(result, matrices[spins[i]][op] )
+    return result
 
 
 def construct_hamiltonian() -> sc.sparse:
     return
 
 
-for i in pauli_matrices(0.5):
-    print( i.toarray() )
+#for i in pauli_matrices(1.0):
+#    print( i.toarray() )
+
+#print( construct_term("II", [0.5, 0.5]).toarray() )
